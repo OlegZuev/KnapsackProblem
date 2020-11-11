bd32bab00023374b4a2455303a17f2df8d1b5e18
OlegZuev <olegase123@gmail.com>
2020-11-11 18:29:19 +0500

    Added comments print_variant and solver

--- KnapsackProblem/KnapsackProblemSolver.cpp
+++ KnapsackProblem/KnapsackProblemSolver.cpp
@@ -2,14 +2,18 @@
 #include <iomanip>
 #include <iostream>
 
+/*
+ * Print some variant of knapsack problem' solution
+ * @param variant - knapsack problem' solution
+ */
 void KnapsackProblemSolver::print_variant(const Variant& variant) {
 	const int output_width = 3;
-	for (Item item : variant.items) {
+	for (const Item& item : variant.items) {
 		std::cout << std::fixed << std::setw(output_width) << item.weight << " ";
 	}
 
 	std::cout << std::endl;
-	for (Item item : variant.items) {
+	for (const Item& item : variant.items) {
 		std::cout << std::fixed << std::setw(output_width) << item.value << " ";
 	}
 
--- KnapsackProblem/KnapsackProblemSolver.h
+++ KnapsackProblem/KnapsackProblemSolver.h
@@ -8,7 +8,13 @@ public:
 	KnapsackProblemSolver(KnapsackProblemSolver&&) = delete;
 	KnapsackProblemSolver& operator=(const KnapsackProblemSolver&&) = delete;
 	virtual ~KnapsackProblemSolver() = default;
-	
+
+	/*
+	 * Calculate solution of knapsack problem
+	 * @param items - set of items with some value and weight
+	 * @param M - max capacity of backpack
+	 * @return some solution of knapsack problem 
+	 */
 	virtual Variant solver(std::vector<Item> items, double M) = 0;
 	static void print_variant(const Variant& variant);
 
